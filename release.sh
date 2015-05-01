84c6a84d492a949184fada5ec4b5e580fdd8621b
Manu <manu.manjunath@flipkart.com>
2015-05-01 17:31:46 +0530

    Version upgraded to v1.0.3

--- release.sh
+++ release.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mvn clean deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
-mvn clean deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
-
+mvn clean deploy -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
